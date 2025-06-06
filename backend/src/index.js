62f71a49e48c3a7f07cf187d8232565dc61d710d
Prince Bajgain <prnncebajgain@gmail.com>
2025-06-06 12:37:27 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,9 +19,9 @@ app.use(cookieParser())
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+    // app.get("*",(req,res)=>{
+    //     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    // })
 }
 
 app.use(cors({
